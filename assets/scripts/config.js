ca9c0b7eac1cff1d92aba86c92df10e484b2ba12
staceyjenkins <hello@staceyjenkins.co.uk>
2020-07-28 02:12:13 +0100

    Fixing JS

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@ requirejs.config({
     paths:{
         disqusloader:'lib/disqusloader-min',
         instagramfeed: 'lib/instagramfeed',
-        jquery: '//code.jquery.com/jquery-3.5.1.slim.min',
+        jquery: '//code.jquery.com/jquery-3.5.1.min',
         fontawesome: '//use.fontawesome.com/releases/v5.0.6/js/all',
         waypoints:'lib/jquery.waypoints.min',
         app: 'app'
